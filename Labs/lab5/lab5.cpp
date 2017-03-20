1a395011080554f7af74eb7995c97edc0748b3ac
Johnny Reina <johnny.r.reina@gmail.com>
2017-03-19 23:11:30 -0500

    Adds completed lab 5.

--- Labs/lab5/lab5.cpp
+++ Labs/lab5/lab5.cpp
@@ -4,21 +4,28 @@
 
 using namespace std;
 
-template<class T> class node
+template <class T>
+class node
 {
   public:
-
     node *next;
+    node *prev;
     T data;
 
-    node(T d) { next = NULL; data = d; }
+    node(T d)
+    {
+        next = NULL;
+        data = d;
+    }
 };
 
-template<class T> class linked_list
+template <class T>
+class linked_list
 {
   private:
     node<T> *head;
     node<T> *tail;
+    node<T> *ptrUsr;
 
   public:
 
@@ -27,27 +34,63 @@ template<class T> class linked_list
         head = NULL;
     }
 
-    T get(int n){
-        if(n < size()){
+    void reset_ptr()
+    {
+        ptrUsr = head;
+    }
+
+    void adv_ptr()
+    {
+        if(!ptrUsr) cout << "ERROR: User pointer is null!" << endl;
+        if(ptrUsr->next) ptrUsr = ptrUsr->next;
+    }
+
+    void rew_ptr()
+    {
+        if(!ptrUsr) {
+            cout << "ERROR: User pointer is null!" << endl;
+            return;
+        }
+        if(ptrUsr->prev) ptrUsr = ptrUsr->prev;
+    }
+
+    T getup()
+    {
+        return ptrUsr->data;
+    }
+
+    T get(int n)
+    {
+        if (n < size())
+        {
             node<T> *tptr = head;
-            for(int i = 0; i < n; i++) tptr=tptr->next;
+            for (int i = 0; i < n; i++)
+                tptr = tptr->next;
             return tptr->data;
         }
-        else return (T)0;
+        else
+            return (T)0;
     }
 
-    T get_first(){
-        if(head) return head->data;
-        else return (T)0;
+    T get_first()
+    {
+        if (head)
+            return head->data;
+        else
+            return (T)0;
     }
 
-    T get_last(){
-        if(head) {
+    T get_last()
+    {
+        if (head)
+        {
             node<T> *ptr = head;
-            while(ptr->next) ptr=ptr->next;
+            while (ptr->next)
+                ptr = ptr->next;
             return ptr->data;
         }
-        else return (T)0;
+        else
+            return (T)0;
     }
 
     int size()
@@ -56,7 +99,7 @@ template<class T> class linked_list
 
         int c = 0;
 
-        while(tptr)
+        while (tptr)
         {
             c++;
             tptr = tptr->next;
@@ -68,31 +111,40 @@ template<class T> class linked_list
     void add(int index, T d)
     {
         int count = size();
-        
-        if(index > count || index < 0) return;
-        if(index == 0) {
+
+        if (index > count || index < 0) return;
+        if (index == 0)
+        {
             node<T> *nhp = new node<T>(d);
             nhp->next = head;
             head = nhp;
+            tail = nhp;
         }
-        else if(index == count) {
+        else if (index == count)
+        {
             node<T> *tptr = head;
             if (head == NULL)
             {
                 head = new node<T>(d);
+                tail = head;
                 return;
             }
             while (tptr->next != NULL) tptr = tptr->next;
             tptr->next = new node<T>(d);
+            tail = tptr->next;
+            tail->prev = tptr;
         }
-        else if(index < count) {
+        else if (index < count)
+        {
             node<T> *tptr = head;
             node<T> *nptr = new node<T>(d);
-            for(int i = 0; i < index - 1; i++) {
+            for (int i = 0; i < index - 1; i++)
+            {
                 tptr = tptr->next;
             }
-            nptr->next=tptr->next;
-            tptr->next=nptr;
+            nptr->next = tptr->next;
+            tptr->next = nptr;
+            tail = tptr->next;
         }
     }
 
@@ -101,12 +153,10 @@ template<class T> class linked_list
         add(0, d);
     }
 
-
     void add_last(T d)
     {
         add(size(), d);
     }
-
 
     void remove_first()
     {
@@ -120,99 +170,143 @@ template<class T> class linked_list
 
     void remove(int n)
     {
-	    if(!head) return;
-        if(n >= size()) return;
-        if(n == 0 && size() == 1){
+        // If list is empty
+        if (!head) return;
+
+        // If n is outside of list
+        if (n >= size()) return;
+
+        // If removing the only node
+        if (n == 0 && size() == 1)
+        {
             delete head;
             head = NULL;
+            tail = NULL;
+            ptrUsr = NULL;
             return;
         }
+
         node<T> *tptr = head;
         node<T> *delptr;
-        
-        if(n==0){
+
+        // If removing the head node
+        if (n == 0)
+        {
             tptr = head;
             head = tptr->next;
+            head->prev = NULL;
             delete tptr;
             return;
         }
 
-        for(int i = 0; i < n - 1; i++) tptr = tptr->next;
+        for (int i = 0; i < n - 1; i++) tptr = tptr->next;
 
         delptr = tptr->next;
-        
-        if(delptr->next) tptr->next = delptr->next;
-	    else tptr->next = NULL;
+
+        if(ptrUsr == tptr){
+            adv_ptr();
+        }
+
+        if (delptr->next) 
+        {
+            if(delptr == ptrUsr) adv_ptr();
+            tptr->next = delptr->next;
+            tptr->next->prev = tptr;
+        }
+        else {
+            if(delptr == ptrUsr) rew_ptr();
+            tptr->next = NULL;
+            tail = tptr;
+        }
 
         delete delptr;
-
     }
     void dump()
     {
         node<T> *tptr;
+        node<T> *hptr;
 
         cout << "    DUMP: (size = " << size() << ", first = " << get_first() << ", last = " << get_last() << ")\n";
 
-        if (head == NULL) {
+        if (head == NULL)
+        {
             cout << "    DUMP: head = NULL\n\n";
             return;
         }
 
-        tptr = head;
+        hptr = head;
+        tptr = tail;
 
-        cout << "    DUMP:   head = : " << tptr->data << endl;
-        while (tptr->next != NULL) {
-            tptr = tptr->next;
-            cout << "    DUMP:   data = : " << tptr->data << endl;
+        cout << "    DUMP:   head = " << hptr->data << "   tail = " << tail->data << endl;
+        while (hptr->next != NULL)
+        {
+            hptr = hptr->next;
+            tptr = tptr->prev;
+            cout << "    DUMP:   data = " << hptr->data << "   data = " << tptr->data << endl;
         }
         cout << endl;
     }
 };
 
-template<class T> class stack
-{
-    private:
-    linked_list<T> ll;
-    T dtop;
-    public:
-
-    void push(T d)
-    {
-        ll.add_first(d);
-        dtop = d;
-    }
-    T pop()
-    {
-        dtop = ll.get_first();
-        if(ll.size() > 0) ll.remove_first();
-        return dtop;
-    }
-    T top()
-    {
-        return ll.get_first();
-    }
-};
-
 int main(void)
 {
-    stack<string> s;
-    int n;
+    linked_list<int> ll;
     string cmd;
-    string d;
+    int d, i;
 
-    while (cin >> cmd >> d) {
+    while (cin >> cmd >> i >> d)
+    {
 
-        cout << "MAIN: cmd = " << cmd << ", d = " << d << endl;
+        cout << "MAIN: cmd = " << cmd << ", index = " << i
+             << ", data = " << d << endl;
 
-        if (cmd == "push")
-            s.push(d);
-        else if (cmd == "pop") {
-            d = s.pop();
-            cout << "MAIN: pop = " << d << endl;
-        } else if (cmd == "top") {     // returns the value from top of stack,
-            d = s.top();        // but DOES NOT REMOVE the item.
-            cout << "MAIN: top = " << d << endl;
-        } else if (cmd == "quit")
+        if (cmd == "add")
+            ll.add(i, d);
+        else if (cmd == "addf")
+            ll.add_first(d);
+        else if (cmd == "addl")
+            ll.add_last(d);
+        else if (cmd == "rem")
+            ll.remove(i);
+        else if (cmd == "remf")
+            ll.remove_first();
+        else if (cmd == "reml")
+            ll.remove_last();
+        else if (cmd == "get")
+        {
+            d = ll.get(i);
+            cout << "get returns: " << d << endl;
+        }
+        else if (cmd == "getf")
+        {
+            d = ll.get_first();
+            cout << "getf returns: " << d << endl;
+        }
+        else if (cmd == "getl")
+        {
+            d = ll.get_last();
+            cout << "getl returns: " << d << endl;
+        }
+        else if (cmd == "reset")
+        { // set user ptr to head of list
+            ll.reset_ptr();
+        }
+        else if (cmd == "adv")
+        { // advance user ptr to next node
+            ll.adv_ptr();
+        }
+        else if (cmd == "rew")
+        { // rewind  user ptr to prev node
+            ll.rew_ptr();
+        }
+        else if (cmd == "getup")
+        { // return value at user ptr node
+            int i = ll.getup();
+            cout << "getup returns: " << i << endl;
+        }
+        else if (cmd == "dump")
+            ll.dump();
+        else if (cmd == "quit")
             exit(0);
     }
 }
